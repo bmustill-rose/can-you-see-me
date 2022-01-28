b188416e925955194a44a435890497e03a3b9bcd
bmustill-rose <services+github@benmr.com>
2022-01-28 16:28:24 +0000

    Attempt to set webcam resolution to 1080P

--- cysm.py
+++ cysm.py
@@ -1,3 +1,9 @@
+import sys
+try:
+ sys.path.append(sys._MEIPASS)
+except:
+ pass
+
 from speechlight import speech
 import cv2
 
@@ -6,6 +12,10 @@ import handlers
 
 speech.output(strings.loadingMessage)
 cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080) #Attempt to set webcam resolution to 1080P. Will / should gracefully fall back to maximum supported if it can't
+
 faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 #VideoCapture won't throw an exception if the device doesn't exist so only cv2-native way of figuring out if a webcam is attached is to try and display an image from it and see what happens
